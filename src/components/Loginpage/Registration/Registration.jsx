7482348dbc921dfdbd8edad058e376a17add7f96
somaiya shahin <somaiyashahin484@gmail.com>
2023-05-03 00:29:59 +0600

    registration page created

--- src/components/Loginpage/Registration/Registration.jsx
+++ src/components/Loginpage/Registration/Registration.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Button, Container, Form } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import slide1 from '../../../assets/slide-1.jpg'
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../providers/AuthProvider';
@@ -12,22 +12,20 @@ const Registration = () => {
         const form = event.target;
         const name = form.name.value;
         const email = form.email.value;
-        const password = form.Password.value;
-        const photo = form.Photo.value;
-        // console.log(name, photo, email, password)
-        createUser (email, password)
-        .then(result => {
+        const password = form.password.value
+        const photo = form.photo.value;
+
+
+        console.log(name, photo, email, password)
+        createUser(email, password)
+        .then(result =>{
             const createdUser = result.user;
-            console.log(createdUser)
+            console.log(createdUser);
         })
         .catch(error =>{
             console.log(error);
         })
     }
-
-
-
-
 
     return (
         <Container className='mt-5 pt-5 h-100'>
@@ -35,20 +33,18 @@ const Registration = () => {
                 <div className="row g-0 text-center">
                     <div className="col-md-6">
                         <div className="card-body  py-4">
-                            <Form onSubmit={handleRegister} className='d-flex flex-column justify-content-center align-items-center mt-5'>
+                            <form onSubmit={handleRegister} className='d-flex flex-column justify-content-center align-items-center mt-5'>
                                 <input className=' mb-4 w-75 py-3 px-2 border-0 shadow' type="text" placeholder=' Your name' name='name' required />
-
                                 <input className=' mb-4 w-75 py-3 px-2 border-0 shadow' type="email" placeholder=' Email' name='email' required />
-
                                 <input className=' mb-4 w-75 py-3 px-2 border-0 shadow' type="password" placeholder='Password' name='password' required />
 
-                                <input className=' mb-4 w-75 py-3 px-2 border-0 shadow' type="text" placeholder='Photo URL' name='Photo' required />
+                                <input className=' mb-4 w-75 py-3 px-2 border-0 shadow' type="text" placeholder='Photo URL' name='photo' required />
 
-                                <Button className=' mb-4 btn btn-success w-25 py-2' >Register</Button>
+                                <button className=' mb-4 btn btn-success w-25 py-2' >Register</button>
                                 {/* <submit ></submit> */}
-                                <Form.Text>Already Have An Account ? Then<Link className='text-success' to="/login"> LogIn </Link> </Form.Text>
+                                <p>Already Have An Account ? Then<Link className='text-success' to="/login"> LogIn </Link> </p>
 
-                            </Form>
+                            </form>
                         </div>
                     </div>
                     <div className="col-md-6 siderbar">
